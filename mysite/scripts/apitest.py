aa2eb97c74a06ad83e70bce91f75b375c6d53d20
Justin Vaughn <vaughnju@oregonstate.edu>
2020-05-15 09:25:47 -0700

    Added comments and cleaned up my scripts.

--- mysite/scripts/apitest.py
+++ mysite/scripts/apitest.py
@@ -6,6 +6,7 @@ import data.strings as creds
 def createKey():
     keyAge = path.getmtime('api_access.txt') if path.exists('api_access.txt') else 0
     now = time.time()
+    #If the last time the file was updated is greater than 24 hours ago, get a new code
     if now - keyAge > 86399:
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
@@ -16,9 +17,10 @@ def createKey():
         'client_id': creds.GetLocationsClientID,
         'client_secret': creds.GetLocationsClientSecret
         }
-
+        #request from OSU oAuth system to get a new toekn
         response = requests.post('https://api.oregonstate.edu/oauth2/token', headers=headers, data=data)
         key = response.json()
+        #save the new token to the text document
         f=open("api_access.txt", "w+")
         f.write("%s" % key['access_token'])
         f.close()
--- mysite/scripts/getLocations.py
+++ mysite/scripts/getLocations.py
@@ -4,22 +4,23 @@ from scripts import apitest
 
 def getLocs():
     apitest.createKey()
-
+    #get most current api key from the api_access.txt file
     f=open('api_access.txt', 'r')
-
     key = f.read()
     f.close()
+    #headers for the request to ensure it is formatted correctly and authorized.
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
         'Authorization': 'Bearer '+key,
     }
-
+    #parameters for the request to the OSU GetLocations API with commented out additional options for filtering
+    #based on the site desired for later development
     params = (
         #('type', 'building'),
         ('page[size]', '10000'),
         #('campus', 'corvallis'),
     )
-
+    #request to OSU's GetLocations API that returns a json object with all the locations and their information
     response = requests.get('https://api.oregonstate.edu/v1/locations', headers=headers, params=params)
     locations = response.json()
 
@@ -34,8 +35,5 @@ def getLocs():
                 'abbr' : x['attributes']['abbreviation'],
             }      
             for x in locations['data']}
-    #f=open('locs.json', 'w')
     out = json.dumps(locs)
     return out
-    #f.write(out)
-    #f.close()
